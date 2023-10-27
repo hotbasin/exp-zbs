f3eec81b63b78afd09915dbbd9d9101c29d62f5b
Stalk Spectrum <stalkspectrum@wildfielded.site>
2023-10-27 14:59:37 +0800

    Workday save5 27-10-2023

--- main.py
+++ main.py
@@ -12,11 +12,11 @@ from fastapi import FastAPI, responses
 
 # Корневой index.html
 ROOT_INDEX_FILE = 'static/index.html'
-
-
 app = FastAPI()
 
-# async def server_root() -> str:
+
+''' =====----- Endpoints -----===== '''
+
 @app.get('/')
 async def server_root() -> str:
     ''' Аналог index.html в ServerRoot для начальной страницы FastAPI
@@ -24,9 +24,6 @@ async def server_root() -> str:
         [str] -- содержимое HTML-файла, заданного в глобальной
             переменной ROOT_INDEX_FILE
     '''
-    # with open(ROOT_INDEX_FILE, 'r', encoding='utf-8') as f_:
-        # return f_.read()
-    # return "Hello World"
     return responses.FileResponse(ROOT_INDEX_FILE)
 
 #####=====----- THE END -----=====#########################################

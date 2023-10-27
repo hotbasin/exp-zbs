58f13bc11d0f88d31807ad0b2c3ee97a4f9fbeea
Stalk Spectrum <stalkspectrum@wildfielded.site>
2023-10-27 14:45:18 +0800

    Workday save3 27-10-2023

--- main.py
+++ main.py
@@ -1,14 +1,32 @@
 #!/usr/bin/python3
 
+#####=====----- TEMPORAL for WinDev -----=====#####
 import sys
 sys.path.append('VENV\\Lib\\site-packages')
+###################################################
 
-from fastapi import FastAPI
+from fastapi import FastAPI, responses
+
+
+''' =====----- Global variables -----===== '''
+
+# Корневой index.html
+ROOT_INDEX_FILE = 'static/index.html'
+
 
 app = FastAPI()
 
+# async def server_root() -> str:
 @app.get('/')
 async def server_root() -> str:
-    return 'It is works'
+    ''' Аналог index.html в ServerRoot для начальной страницы FastAPI
+    Returns:
+        [str] -- содержимое HTML-файла, заданного в глобальной
+            переменной ROOT_INDEX_FILE
+    '''
+    # with open(ROOT_INDEX_FILE, 'r', encoding='utf-8') as f_:
+        # return f_.read()
+    # return "Hello World"
+    return responses.FileResponse(ROOT_INDEX_FILE)
 
 #####=====----- THE END -----=====#########################################

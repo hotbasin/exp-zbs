55eab4b254cd8b4589e8b3d469e4b692520ae73a
Ded Wildfield <wildfielded@wildfielded.site>
2023-10-29 14:16:46 +0800

    Workday save3 29-10-2023

--- test_api.py
+++ test_api.py
@@ -7,4 +7,11 @@ import requests
 # IP-адрес/FQDN и порт тестируемого сервера
 SRV_ADDR = 'localhost:8080'
 
+
+''' =====----- Functions -----===== '''
+
+def test_root() -> str:
+    r = requests.get(f'http://{SRV_ADDR}')
+    return r.text
+
 #####=====----- THE END -----=====#########################################

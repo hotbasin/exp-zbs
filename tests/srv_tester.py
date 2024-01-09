842d24e38518dd9b0fe81bdab22938e0d79ebec0
Stalk Spectrum <stalkspectrum@wildfielded.site>
2024-01-09 16:10:50 +0800

    Workday save2 09-01-2024

--- tests/srv_tester.py
+++ tests/srv_tester.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
 
 import json
+from time import ctime
 
 import requests
 
 
 ''' =====----- Global variables -----===== '''
 
+# Временный файл для хранения access-токена
+TOKEN_FILE = 'acc-token.json'
 # IP-адрес/FQDN и порт тестируемого сервера
 SRV_ADDR = '127.0.0.1:7077'
 # SRV_ADDR = 'zonest.ssszone.ru:7077'
@@ -22,14 +25,50 @@ def test_index(path_: str='') -> str:
 
 # Проверка /srv1/auth/login
 def test_login_post(credentials: dict) -> str:
+    ''' Тест аутентификации на ресурсе сервера.
+    Делает POST-запрос на сервер, login и password вкладывает в json.
+    При удачном ответе в json {status: "success"} записывает полученный
+    access-токен и его время окончания действия (в читаемом формате) во
+    временный файл, указанный в глобальной переменной TOKEN_FILE для
+    использования в других тестах.
+    Arguments:
+        credentials [dict] -- Логин и пароль
+    Returns:
+        [str] -- Отформатированный многострочник со значениями
+            text/acc_token/expired в случае успеха
+            или со значением 'text' при ошибке
+    '''
     response_ = requests.post(f'http://{SRV_ADDR}/srv1/auth/login',
                               json=credentials)
-    return response_.json()
+    ##### return response_.json()
+    response_dict = response_.json()
+    if response_dict['status'] == 'success':
+        text_ = response_dict['text']
+        acc_token_ = response_dict['acc_token']
+        acc_expired_ = response_dict['acc_expired']
+        ref_token_ = response_dict['ref_token']
+        ref_expired_ = response_dict['ref_expired']
+        token_dict = {'acc_token': acc_token_,
+                      'acc_expired': ctime(acc_expired_),
+                      'ref_token': ref_token_,
+                      'ref_expired': ctime(ref_expired_)
+                     }
+        with open(TOKEN_FILE, 'w', encoding='utf-8') as t_:
+            json.dump(token_dict, t_, ensure_ascii=False, indent=4)
+        ret_str = f'Result: {text_}\n' + \
+                  f'Access-token: {acc_token_}\n' + \
+                  f'Expired: {ctime(acc_expired_)}\n' + \
+                  f'Refresh-token: {ref_token_}\n' + \
+                  f'Expired: {ref_expired_}'
+        return ret_str
+    else:
+        # return f"FAIL: {response_dict['text']}"
+        return response_.json()
 
 
 # Проверка /srv2/auth/login
 def test_login_get(credentials: dict) -> str:
-    response_ = requests.get(f'http://{SRV_ADDR}/srv2/auth/login',
+    response_ = requests.get(f'http://{SRV_ADDR}/srv1/auth/login',
                              params=credentials)
     return response_.json()
 

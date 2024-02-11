f13dd3931d15047037dc2913ab02f4dd04ade527
Ded Wildfield <wildfielded@wildfielded.site>
2024-02-11 21:14:14 +0800

    Workday save2 11-02-2024

--- ds_model/preprocessing.py
+++ ds_model/preprocessing.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
 
+import pandas as pd
 
+EXCEL_FILE = 'data/anketa_demo.xlsx'
+CSV_FILE = 'data/anketa_demo.csv'
+
+demo_df = pd.read_excel(EXCEL_FILE)
 
 #####=====----- THE END -----=====#########################################

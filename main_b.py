2fbc52d851237c867dbb1bc1418c61994772f969
Ded Wildfield <wildfielded@wildfielded.site>
2024-02-14 23:30:20 +0800

    Workday save6 14-02-2024

--- main_b.py
+++ main_b.py
@@ -16,9 +16,10 @@ from pathlib import Path
 from time import time
 from typing import Annotated
 
-from fastapi import FastAPI, responses, File, UploadFile, Depends
+from fastapi import (FastAPI, responses, File, UploadFile,
+                     Depends, HTTPException, status)
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.security import OAuth2PasswordBearer
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from pydantic import BaseModel
 import uvicorn
 
@@ -38,6 +39,41 @@ srv.add_middleware(
 )
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl='token')
 
+'''
+fake_users_db = {
+    'user1': {
+        'username': 'User One',
+        'login': 'user1',
+        'password': 'qwerty1'
+    },
+    'user2': {
+        'username': 'User Two',
+        'login': 'user2',
+        'password': 'qwerty2'
+    },
+    user3: {
+        'username': 'User Three',
+        'login': 'user3',
+        'password': 'qwerty3'
+    }
+}
+'''
+fake_users_db = {
+    "johndoe": {
+        "username": "johndoe",
+        "full_name": "John Doe",
+        "email": "johndoe@example.com",
+        "hashed_password": "fakehashedsecret",
+        "disabled": False,
+    },
+    "alice": {
+        "username": "alice",
+        "full_name": "Alice Wonderson",
+        "email": "alice@example.com",
+        "hashed_password": "fakehashedsecret2",
+        "disabled": True,
+    },
+}
 
 ''' =====----- Classes -----===== '''
 
@@ -55,15 +91,36 @@ class User(BaseModel):
     email: str | None = None
     full_name: str | None = None
     disabled: bool | None = None
+class UserInDB(User):
+    hashed_password: str
 
 
 ''' =====----- Functions -----===== '''
 
+def fake_hash_password(password: str):
+    return 'fakehashed' + password
+def get_user(db, username: str):
+    if username in db:
+        user_dict = db[username]
+        return UserInDB(**user_dict)
+
 def fake_decode_token(token):
-    return User(username=token+'fakedecoded', email='ss@dd.ru', full_name='Stalk Spectrum')
+    # return User(username=token+'fakedecoded', email='ss@dd.ru', full_name='Stalk Spectrum')
+    user = get_user(fake_users_db, token)
+    return user
 async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
     user = fake_decode_token(token)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail='Invalid credentials',
+            headers={'WWW-Authenticate': 'Bearer'}
+        )
     return user
+async def get_current_active_user(current_user: Annotated[User, Depends(get_current_user)]):
+    if current_user.disabled:
+        raise HTTPException(status_code=400, detail='Inactive user')
+    return current_user
 
 
 ''' =====----- Endpoints -----===== '''
@@ -81,8 +138,18 @@ async def server_root() -> str:
 @srv.get('/items/')
 async def read_items(token: Annotated[str, Depends(oauth2_scheme)]):
     return {'token': token}
-@srv.get('users/me')
-async def read_users_me(current_user: Annotated[User, Depends(get_current_user)]):
+@srv.post('/token')
+async def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
+    user_dict = fake_users_db.get(form_data.username)
+    if not user_dict:
+        raise HTTPException(status_code=400, detail='Incorrect user or passwd')
+    user = UserInDB(**user_dict)
+    hashed_password = fake_hash_password(form_data.password)
+    if hashed_password != user.hashed_password:
+        raise HTTPException(status_code=400, detail='Incorrect user or passwd')
+    return {'acc_token': user.username, 'token_type': 'bearer'}
+@srv.get('/users/me')
+async def read_users_me(current_user: Annotated[User, Depends(get_current_active_user)]):
     return current_user
 
 

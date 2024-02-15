b7b4405ef092548c7541aced096cc004897a5109
Stalk Spectrum <stalkspectrum@wildfielded.site>
2024-02-15 22:47:48 +0800

    Workday save2 15-02-2024

--- main_b.py
+++ main_b.py
@@ -7,13 +7,13 @@ https://fastapi.tiangolo.com/ru/tutorial/security/first-steps/
 #####=====----- TEMPORAL for VENV -----=====#####
 import sys
 sys.path.append('VENV/Lib/site-packages')
-sys.path.append('VENV/lib/python3.10/site-packages')
-sys.path.append('VENV/lib/python3.11/site-packages')
-sys.path.append('~/.local/bin')
+# sys.path.append('VENV/lib/python3.10/site-packages')
+# sys.path.append('VENV/lib/python3.11/site-packages')
+# sys.path.append('~/.local/bin')
 #################################################
 
 from pathlib import Path
-from time import time
+# from time import time
 from typing import Annotated
 
 from fastapi import (FastAPI, responses, File, UploadFile,
@@ -26,122 +26,77 @@ import uvicorn
 import srv_api_a as api_
 import set_env as e_
 
-
-''' =====----- Global variables -----===== '''
-
-srv = FastAPI()
-'''
-srv.add_middleware(
-    CORSMiddleware,
-    allow_origins=['*'],
-    allow_credentials=True,
-    allow_methods=['*'],
-    allow_headers=['*']
-)
-'''
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl='token')
-
-'''
 fake_users_db = {
-    'user1': {
-        'username': 'User One',
-        'login': 'user1',
-        'password': 'qwerty1'
-    },
-    'user2': {
-        'username': 'User Two',
-        'login': 'user2',
-        'password': 'qwerty2'
-    },
-    user3: {
-        'username': 'User Three',
-        'login': 'user3',
-        'password': 'qwerty3'
-    }
-}
-'''
-fake_users_db = {
-    "johndoe": {
-        "username": "johndoe",
+    "john": {
+        "username": "john",
         "full_name": "John Doe",
         "email": "johndoe@example.com",
-        "hashed_password": "fakehashedsecret",
+        "hashed_password": "secret",
         "disabled": False,
     },
     "alice": {
         "username": "alice",
         "full_name": "Alice Wonderson",
         "email": "alice@example.com",
-        "hashed_password": "fakehashedsecret2",
+        "hashed_password": "secret2",
         "disabled": True,
     },
 }
 
-''' =====----- Classes -----===== '''
+srv = FastAPI()
 
-'''
-class Credentials(BaseModel):
-    login: str
-    password: str
-'''
+def fake_hash_password(password: str):
+    # return 'fakehashed' + password
+    return password
 
-'''
-class RefreshToken(BaseModel):
-    ref_token: str
-'''
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl='token')
 
 class User(BaseModel):
     username: str
     email: str | None = None
     full_name: str | None = None
     disabled: bool | None = None
+
 class UserInDB(User):
     hashed_password: str
 
-
-''' =====----- Functions -----===== '''
-
-def fake_hash_password(password: str):
-    return 'fakehashed' + password
 def get_user(db, username: str):
     if username in db:
         user_dict = db[username]
         return UserInDB(**user_dict)
 
 def fake_decode_token(token):
-    # return User(username=token+'fakedecoded', email='ss@dd.ru', full_name='Stalk Spectrum')
     user = get_user(fake_users_db, token)
     return user
+
 async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
     user = fake_decode_token(token)
     if not user:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
-            detail='Invalid credentials',
+            detail='Invalid credentials in get_current_user',
             headers={'WWW-Authenticate': 'Bearer'}
         )
     return user
+
 async def get_current_active_user(current_user: Annotated[User, Depends(get_current_user)]):
     if current_user.disabled:
         raise HTTPException(status_code=400, detail='Inactive user')
     return current_user
 
-
-''' =====----- Endpoints -----===== '''
-
-@srv.get('/')
-async def server_root() -> str:
+# @srv.get('/')
+# async def server_root() -> str:
     ''' Аналог index.html в ServerRoot для начальной страницы FastAPI
     Returns:
         [str] -- содержимое HTML-файла, заданного в глобальной
             переменной ROOT_INDEX_FILE
     '''
-    return responses.FileResponse(e_.ROOT_INDEX_FILE)
+#     return responses.FileResponse(e_.ROOT_INDEX_FILE)
 
+# @srv.get('/items/')
+# async def read_items(token: Annotated[str, Depends(oauth2_scheme)]):
+#     return {'token': token}
 
-@srv.get('/items/')
-async def read_items(token: Annotated[str, Depends(oauth2_scheme)]):
-    return {'token': token}
 @srv.post('/token')
 async def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
     user_dict = fake_users_db.get(form_data.username)
@@ -152,6 +107,7 @@ async def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
     if hashed_password != user.hashed_password:
         raise HTTPException(status_code=400, detail='Incorrect user or passwd')
     return {'acc_token': user.username, 'token_type': 'bearer'}
+
 @srv.get('/users/me')
 async def read_users_me(current_user: Annotated[User, Depends(get_current_active_user)]):
     return current_user
